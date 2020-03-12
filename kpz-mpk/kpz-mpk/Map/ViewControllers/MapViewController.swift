3aebfe8998d1af3933b4ff3cea361723bdb5e26c
wojtek717 <wojciechkonury@gmail.com>
2020-03-12 11:36:59 +0100

    MapView Implementation, Map configuration, Map starting point

--- kpz-mpk/kpz-mpk/Map/ViewControllers/MapViewController.swift
+++ kpz-mpk/kpz-mpk/Map/ViewControllers/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  kpz-mpk
 //
 //  Created by Wojciech Konury on 02/03/2020.
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MapKit
+
+protocol MapViewControllerPresenter: NSObject {
+  func centerMap(coordinateRegion: MKCoordinateRegion)
+}
 
 class MapViewController: UIViewController {
 
+  @IBOutlet private weak var mapView: MKMapView!
+
+  lazy var viewModel = MapViewModel(presenter: self)
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    viewModel.centerMap(atLatitude: 51.111510, atLongitude: 17.059737, forRegionRadious: 1000)
   }
 
+}
+
+extension MapViewController: MapViewControllerPresenter {
+  func centerMap(coordinateRegion: MKCoordinateRegion) {
+    mapView.setRegion(coordinateRegion, animated: true)
+  }
 }
--- kpz-mpk/kpz-mpk/Map/ViewModels/MapViewModel.swift
+++ kpz-mpk/kpz-mpk/Map/ViewModels/MapViewModel.swift
@@ -7,6 +7,25 @@
 //
 
 import Foundation
+import MapKit
 
 final class MapViewModel {
+  private weak var presenter: MapViewControllerPresenter?
+
+  init(presenter: MapViewControllerPresenter?) {
+    self.presenter = presenter
+  }
+
+  func centerMap(
+    atLatitude lat: Double,
+    atLongitude long: Double,
+    forRegionRadious radious: Double) {
+    let location = CLLocation(latitude: lat, longitude: long)
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: radious,
+      longitudinalMeters: radious)
+
+    presenter?.centerMap(coordinateRegion: coordinateRegion)
+  }
 }
