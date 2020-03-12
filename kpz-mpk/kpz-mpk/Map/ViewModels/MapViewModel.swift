4125e530778b8e1b9bbc862c777a9d5f0961ae79
wojtek717 <wojciechkonury@gmail.com>
2020-03-12 10:54:19 +0100

    Map folder structure

---
+++ kpz-mpk/kpz-mpk/Map/ViewModels/MapViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MapViewModel.swift
+//  kpz-mpk
+//
+//  Created by Wojciech Konury on 12/03/2020.
+//  Copyright © 2020 kpz-mpk. All rights reserved.
+//
+
+import Foundation
+
+final class MapViewModel {
+}
