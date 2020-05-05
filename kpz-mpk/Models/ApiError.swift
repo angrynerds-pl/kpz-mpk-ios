35dac21d5aba2e0c3c0c0032b6b8a8dfeea85f86
wojtek717 <wojciechkonury@gmail.com>
2020-05-05 20:25:58 +0200

    add ApiError

---
+++ kpz-mpk/Models/ApiError.swift
@@ -0,0 +1,15 @@
+//
+//  ApiError.swift
+//  kpz-mpk
+//
+//  Created by Wojciech Konury on 05/05/2020.
+//  Copyright © 2020 kpz-mpk. All rights reserved.
+//
+
+import Foundation
+
+struct ApiError {
+  var statusCode: Int
+  var error: String
+  var message: String
+}
