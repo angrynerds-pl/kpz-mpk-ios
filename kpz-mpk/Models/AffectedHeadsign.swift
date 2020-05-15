5e0a04165358ff09b4241306800c8b6056ab1bc1
wojtek717 <wojciechkonury@gmail.com>
2020-05-15 17:21:51 +0200

    update Incident model

---
+++ kpz-mpk/Models/AffectedHeadsign.swift
@@ -0,0 +1,15 @@
+//
+//  AffectedHeadsign.swift
+//  kpz-mpk
+//
+//  Created by Wojciech Konury on 15/05/2020.
+//  Copyright © 2020 kpz-mpk. All rights reserved.
+//
+
+import Foundation
+
+struct AffectedHeadsign: Codable {
+  let incidentId: String
+  let routeId: String
+  let tripHeadsign: String
+}
